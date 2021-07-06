3d088268d074cc1477ad84c3a371cc3693739a17
Chubak <chubakbidpaa@gmail.com>
2021-07-06 16:18:04 +0430

    file removed

--- test_db.py
+++
@@ -1,9 +0,0 @@
-from scripts.database_op.db_op import *
-from scripts.utils.log_to_file import *
-
-open_log_file()
-
-connect_to_db()
-#insert_to_db("RECO_ID_1425", "juniperhollow", "/srt2/2424rwe/sfsfsdf")
-print(select_from_db("RECO_ID_1425"))
-close_log_file()
