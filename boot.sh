c58eb04019b34c6aa2765a5545886ab33997e06e
Chubak <chubakbidpaa@gmail.com>
2021-08-09 16:41:33 +0430

    push

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gunicorn -b :5000 --access-logfile --timeout 800 - --error-logfile - faceapp:app
+exec gunicorn -b :5000 --access-logfile - --timeout 800 --error-logfile - faceapp:app
